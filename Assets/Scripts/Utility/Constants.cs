1a6b3977a479fbca2184c73296651cd40b2fe2a4
Quan Nguyen <quan.0512@hotmail.com>
2024-03-01 23:19:37 +0700

    Step 2: Remove unused assets and code

--- Assets/Scripts/Utility/Constants.cs
+++ Assets/Scripts/Utility/Constants.cs
@@ -3,10 +3,4 @@
     public const string GAME_SETTINGS_PATH = "gamesettings";
 
     public const string PREFAB_CELL_BACKGROUND = "prefabs/cellBackground";
-
-    public const string PREFAB_BONUS_HORIZONTAL = "prefabs/itemBonusHorizontal";
-
-    public const string PREFAB_BONUS_VERTICAL = "prefabs/itemBonusVertical";
-
-    public const string PREFAB_BONUS_BOMB = "prefabs/itemBonusBomb";
 }
